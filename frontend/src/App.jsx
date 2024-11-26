3b353d101fe2837346e8d64b892ef9dd42aa404c
Marco Martínez <marcoerick642@gmail.com>
2024-11-26 08:13:41 -0500

    actualizacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './components/AuthContext/AuthContext'; 
+// import { AuthProvider } from './components/AuthContext/AuthContext';
 import HomeComponent from './components/homeComponent/HomeComponent';
 
-import Admins from './components/panels/admins';
+import Panel from './components/Panel/Panel';
 import Docentes from './components/panels/docentes';
 import Alumnos from './components/panels/alumnos';
-import FormAdmin from './components/formLogin/LoginAdmin';
-import OpcionesComponent from './components/opciones/Opciones';
+// import FormAdmin from './components/formLogin/LoginAdmin';
+// import OpcionesComponent from './components/opciones/Opciones';
 
 import { AuthProvider } from './components/AuthContext/AuthContext'; 
-import Panel from './components/Panel/Panel';
+// import Panel from './components/Panel/Panel';
 
-import CourseForm from './components/opciones/Course';
-import UserForm from './components/userRegister/UserRegister';
+// import CourseForm from './components/opciones/Course';
+// import UserForm from './components/userRegister/UserRegister';
 
 function App() {
   return (
@@ -22,15 +22,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomeComponent />} />
-
-          <Route path="/panel-admin" element={<Admins />} />
+          <Route path="/panel" element={<Panel />} />
           <Route path="/panel-docente" element={<Docentes />} />
           <Route path="/panel-alumno" element={<Alumnos />} />
           {/* <Route path="/loginadmin" element={<FormAdmin />} />
           <Route path="/register" element={<OpcionesComponent />} />
-
           <Route path="/panel" element={<Panel/>} />
-
           <Route path="/course-register"element={<CourseForm/>}/>
           <Route path="/registrarse" element={<UserForm/>}/> */}
         </Routes>
